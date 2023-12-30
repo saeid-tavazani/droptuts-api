f4e0302f1dd84b45035b95d61f68f74adafeb763
saeid tavazani <saeidtavazani@gmail.com>
2023-12-30 14:29:16 -0800

    fix headline controller

--- app/controllers/productsController.js
+++ app/controllers/productsController.js
@@ -5,6 +5,8 @@ const {
   updateProducts,
   newHeadline,
   selectHeadline,
+  deleteHeadline,
+  updateHeadline,
 } = require("../models/productsModels");
 const logger = require("../services/errorLogger");
 const { StatusCodes, getReasonPhrase } = require("http-status-codes");
@@ -143,6 +145,70 @@ exports.newHeadline = (req, res, next) => {
           res
             .status(StatusCodes.NOT_IMPLEMENTED)
             .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+        }
+      })
+      .catch((error) => {
+        logger.error(error);
+        res
+          .status(StatusCodes.NOT_IMPLEMENTED)
+          .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+      });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+
+exports.deleteHeadline = (req, res, next) => {
+  try {
+    const { id } = req.body;
+    deleteHeadline([id])
+      .then((row) => {
+        if (row.affectedRows) {
+          selectHeadline().then((headline) => {
+            res.status(StatusCodes.OK).send({
+              success: true,
+              data: headline,
+              message: getReasonPhrase(StatusCodes.OK),
+            });
+          });
+        } else {
+          res.status(StatusCodes.UPGRADE_REQUIRED).send({
+            success: false,
+            message: getReasonPhrase(StatusCodes.UPGRADE_REQUIRED),
+          });
+        }
+      })
+      .catch((error) => {
+        logger.error(error);
+        res
+          .status(StatusCodes.NOT_IMPLEMENTED)
+          .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+      });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+
+exports.updateHeadline = (req, res, next) => {
+  try {
+    const { title, description, poster, price, discount, id } = req.body;
+    updateHeadline([id, title])
+      .then((row) => {
+        if (row.affectedRows) {
+          selectHeadline().then((headline) => {
+            res.status(StatusCodes.OK).send({
+              success: true,
+              data: headline,
+              message: getReasonPhrase(StatusCodes.OK),
+            });
+          });
+        } else {
+          res.status(StatusCodes.UPGRADE_REQUIRED).send({
+            success: false,
+            message: getReasonPhrase(StatusCodes.UPGRADE_REQUIRED),
+          });
         }
       })
       .catch((error) => {
