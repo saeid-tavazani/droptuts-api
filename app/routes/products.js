e2613bed1fe31958adcd863de05b0a212c87fd9b
saeid tavazani <saeidtavazani@gmail.com>
2023-12-30 06:39:05 -0800

    add route for new products

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
-const {
-  validator,
-  emailValidator,
-  passValidator,
-} = require("../middlewares/validator");
-const {
-  newSession,
-  verifyToken,
-} = require("../controllers/sessionsController");
+const { validator, customMadeValidator } = require("../middlewares/validator");
+const { newProducts } = require("../controllers/productsController");
 
 const router = express.Router();
 
-router.post("/new", [auth], verifyToken);
+router.post(
+  "/new",
+  [
+    auth,
+    validator([
+      customMadeValidator("title").notEmpty(),
+      customMadeValidator("description").notEmpty(),
+      customMadeValidator("poster").notEmpty(),
+      customMadeValidator("price").optional().isInt(),
+      customMadeValidator("discount").optional().isInt(),
+    ]),
+  ],
+  newProducts
+);
 
 module.exports = router;
