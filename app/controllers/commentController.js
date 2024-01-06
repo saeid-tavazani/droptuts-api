455d6034adb6912fb78eebebac1344480525f745
saeid-tavazani <saeidtavazani@gmail.com>
2024-01-06 03:50:07 -0800

    add comment controller

--- app/controllers/commentController.js
+++ app/controllers/commentController.js
@@ -1,21 +1,23 @@
 const {
-  newOrders,
-  selectOrders,
-  deleteOrders,
-} = require("../models/orderModels");
+  activeComment,
+  addreplyComment,
+  deleteComment,
+  getComment,
+  newComment,
+} = require("../models/commentModels");
 const logger = require("../services/errorLogger");
 const { StatusCodes, getReasonPhrase } = require("http-status-codes");
 
-exports.newOrders = (req, res, next) => {
+exports.newComment = (req, res, next) => {
   try {
-    const { userId, productId } = req.body;
-    newOrders([productId, userId])
+    const { userId, productId, description } = req.body;
+    newComment([userId, productId, description])
       .then((row) => {
         if (row.affectedRows) {
-          selectOrders([userId]).then((order) => {
+          getComment([productId]).then((comment) => {
             res.status(StatusCodes.OK).send({
               success: true,
-              data: order,
+              data: comment,
               message: getReasonPhrase(StatusCodes.OK),
             });
           });
@@ -37,14 +39,14 @@ exports.newOrders = (req, res, next) => {
   }
 };
 
-exports.getOrders = (req, res, next) => {
+exports.getComment = (req, res, next) => {
   try {
     const { id } = req.body;
-    selectOrders([id])
-      .then((order) => {
+    getComment([id])
+      .then((comment) => {
         res.status(StatusCodes.OK).send({
           success: true,
-          data: order,
+          data: comment,
           message: getReasonPhrase(StatusCodes.OK),
         });
       })
@@ -60,15 +62,68 @@ exports.getOrders = (req, res, next) => {
   }
 };
 
-exports.deleteOrders = (req, res, next) => {
+exports.deleteComment = (req, res, next) => {
   try {
     const { id } = req.body;
-    deleteOrders([id])
-      .then((order) => {
+    deleteComment([id])
+      .then((comment) => {
         res.status(StatusCodes.OK).send({
           success: true,
           message: getReasonPhrase(StatusCodes.OK),
         });
+      })
+      .catch((error) => {
+        logger.error(error);
+        res
+          .status(StatusCodes.NOT_IMPLEMENTED)
+          .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+      });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+
+exports.activeComment = (req, res, next) => {
+  try {
+    const { id } = req.body;
+    activeComment([id])
+      .then((comment) => {
+        res.status(StatusCodes.OK).send({
+          success: true,
+          message: getReasonPhrase(StatusCodes.OK),
+        });
+      })
+      .catch((error) => {
+        logger.error(error);
+        res
+          .status(StatusCodes.NOT_IMPLEMENTED)
+          .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+      });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+
+exports.addreplyComment = (req, res, next) => {
+  try {
+    const { userId, productId, description, commentId } = req.body;
+    addreplyComment([userId, productId, description, commentId])
+      .then((row) => {
+        if (row.affectedRows) {
+          getComment([productId]).then((comment) => {
+            res.status(StatusCodes.OK).send({
+              success: true,
+              data: comment,
+              message: getReasonPhrase(StatusCodes.OK),
+            });
+          });
+        } else {
+          res
+            .status(StatusCodes.NOT_IMPLEMENTED)
+            .send({ code: StatusCodes.NOT_IMPLEMENTED, success: false });
+        }
       })
       .catch((error) => {
         logger.error(error);
