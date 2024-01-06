a7c9e0a08521b17b707ca4ecba93528831b11c77
saeid-tavazani <saeidtavazani@gmail.com>
2024-01-06 03:58:09 -0800

    add comment routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,12 @@ const sessionRouter = require("./sessions");
 const usersRouter = require("./users");
 const productsRouter = require("./products");
 const orderRouter = require("./order");
+const commentRouter = require("./comment");
 
 module.exports = (app) => {
   app.use("/app/droptuts/api/v1/session", sessionRouter);
   app.use("/app/droptuts/api/v1/user", usersRouter);
   app.use("/app/droptuts/api/v1/product", productsRouter);
   app.use("/app/droptuts/api/v1/order", orderRouter);
+  app.use("/app/droptuts/api/v1/comment", commentRouter);
 };
